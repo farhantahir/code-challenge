f87f91b5665a56a194254a9f6b9b1e4be771df7d
farhan <mf.tahir@outlook.com>
2018-08-22 04:10:46 +0500

    Added JSDoc Blocks

--- app.js
+++ app.js
@@ -13,25 +13,31 @@ if (config.logger) {
   app.use(config.logger);
 }
 
-
-
-// Body Parsing middleware settings.
+/**
+ * Body Parsing middleware settings.
+ */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Global middleware for setting response headers
+/**
+ * Global middleware for setting response headers.
+ */
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// Global middleware to catch 404 errors
+/**
+ * Global middleware to catch 404 errors.
+ */
 app.use(function (req, res, next) {
   res.status(404).end();
 });
 
-// Global middleware to handle errors
+/**
+ * Global middleware to handle errors.
+ */
 app.use(function (err, req, res, next) {
   res.status(500).send({
     error: err.toString()
