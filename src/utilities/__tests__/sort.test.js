8c938483f1b0214349fc7fe420f63815594f4a41
farhan <mf.tahir@outlook.com>
2018-09-02 00:15:39 +0500

    sort utility test refactored

--- src/utilities/__tests__/sort.test.js
+++ src/utilities/__tests__/sort.test.js
@@ -1,55 +1,53 @@
 const { strSort, numSort } = require('../sort');
 
-const data = [
+const DATA = [
   { "name": "Rotana Hotel", "price": 80.6 },
   { "name": "Media One Hotel", "price": 102.2 },
 ];
 
+
+const EXPECTED_STR_ASC_NUM_DESC = [
+  { "name": "Media One Hotel", "price": 102.2 },
+  { "name": "Rotana Hotel", "price": 80.6 },
+];
+
+const EXPECTED_STR_DESC_NUM_ASC = [
+  { "name": "Rotana Hotel", "price": 80.6 },
+  { "name": "Media One Hotel", "price": 102.2 }
+];
 
 describe('sort array of objects by string or number type field', () => {
 
   /**
    * Sorts array of objects by a string field in asc order
    */
-  test('Sorts objects by string field', () => {
-    const expected = [
-      { "name": "Media One Hotel", "price": 102.2 },
-      { "name": "Rotana Hotel", "price": 80.6 },
-    ];
-    expect(strSort(data, 'name')).toEqual(expect.arrayContaining(expected));
+  test('Sorts objects by string field in ASC order', () => {
+    expect(strSort(DATA, 'name'))
+      .toEqual(expect.arrayContaining(EXPECTED_STR_ASC_NUM_DESC));
   });
 
   /**
    * Sorts array of objects by a string field in desc order
    */
-  test('Sorts objects by string field', () => {
-    const expected = [
-      { "name": "Rotana Hotel", "price": 80.6 },
-      { "name": "Media One Hotel", "price": 102.2 }
-    ];
-    expect(strSort(data, 'name', 'desc')).toEqual(expect.arrayContaining(expected));
+  test('Sorts objects by string field in DESC order', () => {
+    expect(strSort(DATA, 'name', 'desc'))
+      .toEqual(expect.arrayContaining(EXPECTED_STR_DESC_NUM_ASC));
   });
 
   /**
    * Sorts array of objects by number field in asc
    */
-  test('Sorts objects by number field', () => {
-    const expected = [
-      { "name": "Rotana Hotel", "price": 80.6 },
-      { "name": "Media One Hotel", "price": 102.2 }
-    ];
-    expect(numSort(data, 'name')).toEqual(expect.arrayContaining(expected));
+  test('Sorts objects by number field in ASC', () => {
+    expect(numSort(DATA, 'name'))
+      .toEqual(expect.arrayContaining(EXPECTED_STR_DESC_NUM_ASC));
   });
 
   /**
    * Sorts array of objects by number field in desc order
    */
-  test('Sorts objects by number field', () => {
-    const expected = [
-      { "name": "Media One Hotel", "price": 102.2 },
-      { "name": "Rotana Hotel", "price": 80.6 }
-    ];
-    expect(numSort(data, 'name', 'desc')).toEqual(expect.arrayContaining(expected));
+  test('Sorts objects by number field in DESC order', () => {
+    expect(numSort(DATA, 'name', 'desc'))
+      .toEqual(expect.arrayContaining(EXPECTED_STR_ASC_NUM_DESC));
   });
 
 });
